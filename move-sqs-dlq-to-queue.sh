b145f298b1a058779b83de431aa9eadf244a8fb4
barretta <rbarretta@gmail.com>
2019-02-07 08:48:33 -0200

    Update move-sqs-dlq-to-queue.sh

--- move-sqs-dlq-to-queue.sh
+++ move-sqs-dlq-to-queue.sh
@@ -10,6 +10,6 @@ MESSAGE_ID=$(echo $QUEUE_MESSAGE | jq -r '.Messages[0].ReceiptHandle')
 echo $MESSAGE_BODY
 echo $MESSAGE_ID
 
-aws sqs send-message --queue-url "$QUEUE_URL" --message-body $MESSAGE_BODY --region sa-east-1
+aws sqs send-message --queue-url "$QUEUE_URL" --message-body "$MESSAGE_BODY" --region sa-east-1
 
-aws sqs delete-message --queue-url $QUEUE_DLQ_URL --receipt-handle $MESSAGE_ID --region sa-east-1
+aws sqs delete-message --queue-url "$QUEUE_DLQ_URL" --receipt-handle "$MESSAGE_ID" --region sa-east-1
